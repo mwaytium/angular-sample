24ba19fb0ef4910e0c557388a4e8607dd006fab0
mwaytium <makcimqwerty123@gmail.com>
2019-07-14 21:24:27 +0400

    Milestone 3

---
+++ src/app/heroes/hero-list/hero-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-list',
+  templateUrl: './hero-list.component.html',
+  styleUrls: ['./hero-list.component.css']
+})
+export class HeroListComponent implements OnInit {
+
+  selectedId: number;
+
+  heroes$: Observable<Hero[]>;
+
+  constructor(private service: HeroService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        this.selectedId = +params.get('id');
+        return this.service.getHeroes();
+      })
+    );
+  }
+}
